4e5890fc28ab9eb4a634b078f00f10e08c47ab84
euichan0927 <yuichan99@naver.com>
2023-09-12 01:53:15 +0900

    5장 commit

--- src/main/java/com/example/JavaSpringPractice/entity/Article.java
+++ src/main/java/com/example/JavaSpringPractice/entity/Article.java
@@ -5,8 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
-
+@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Entity
